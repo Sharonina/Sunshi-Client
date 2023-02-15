f47140b5b8d59d4f3be262a64f3f8f6360f92d04
Sharon Arana <shar_mdza@hotmail.com>
2023-02-15 16:07:29 -0600

    layout styles

--- src/layouts/Layout.component.jsx
+++ src/layouts/Layout.component.jsx
@@ -1,11 +1,43 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import styles from "./Layout.module.styl";
+import logo from "../assets/sunshi2.png";
+import { routes } from "../utils/constants/routes";
 
 const Layout = () => {
+  const today = new Date();
   return (
     <div data-testid="layout-container">
-      <h1>Layout</h1>
-      <Outlet />
+      <main>
+        <header>
+          <div className={styles.headerSup}>
+            <figure>
+              <img src={logo} />
+            </figure>
+            <div className={styles.headerSupRight}>
+              <div>
+                <p className={styles.date}>
+                  {today.toLocaleString("en-eu", {
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                  })}
+                </p>
+                <p className={styles.userName}>Sharoninadmin</p>
+              </div>
+              <button>btn</button>
+            </div>
+          </div>
+          <nav>
+            <Link to={routes.HOME}>Home</Link>
+            <Link to={routes.PRODUCTS}>Products</Link>
+            <Link to={routes.ORDERS}>Orders</Link>
+            <Link to={routes.USERS}>Users</Link>
+          </nav>
+        </header>
+        <Outlet />
+      </main>
     </div>
   );
 };
