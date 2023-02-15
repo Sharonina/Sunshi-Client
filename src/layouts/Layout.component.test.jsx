1701756694b72e17e588ba166546aef63d7d2b0f
Sharon Arana <shar_mdza@hotmail.com>
2023-02-15 17:40:03 -0600

    test for layout

--- src/layouts/Layout.component.test.jsx
+++ src/layouts/Layout.component.test.jsx
@@ -1,9 +1,15 @@
 import { screen, render } from "@/test/utils";
 import Layout from "./Layout.component";
+import { MemoryRouter } from "react-router-dom";
+import { routes } from "@/utils/constants/routes";
 
 describe("Layout", () => {
   it("should render the layout", () => {
-    render(<Layout />);
+    render(
+      <MemoryRouter initialEntries={[routes.HOME]}>
+        <Layout />
+      </MemoryRouter>
+    );
     expect(screen.getByTestId("layout-container")).toBeDefined();
   });
 });
