a5dce0f4ee6980ff983083f3bc7d7d80884dbd40
Sharon Arana <shar_mdza@hotmail.com>
2023-02-16 11:54:20 -0600

    add styles for home component

--- src/pages/home/Home.component.jsx
+++ src/pages/home/Home.component.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import sushiImg from "@/assets/nigiri.png";
+import styles from "./Home.module.styl";
 
 const Home = () => {
   return (
     <section data-testid="home-page">
-      <figure>
-        <img src={sushiImg} alt="" />
-      </figure>
-      <h1>Hi Sharonina!</h1>
-      <p>
-        Let's create a fantastic dish for our customers sharing our passion for
-        food, culture and innovation.
-      </p>
-      <p>Thank you for being a part of our team!</p>
+      <div>
+        <figure>
+          <img src={sushiImg} alt="" />
+        </figure>
+        <p className={styles.userGreeting}>Hi Sharonina!</p>
+        <p>
+          Let's create a fantastic dish for our customers sharing our passion
+          for food, culture and innovation.
+        </p>
+        <p className={styles.thanks}>Thank you for being a part of our team!</p>
+      </div>
     </section>
   );
 };
