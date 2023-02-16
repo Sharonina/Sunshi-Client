af249b294adc338574f2e3988135a3d228b296f1
Sharon Arana <shar_mdza@hotmail.com>
2023-02-16 10:16:07 -0600

    add home structure

--- src/pages/home/Home.component.jsx
+++ src/pages/home/Home.component.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import sushiImg from "@/assets/nigiri.png";
 
 const Home = () => {
-  return <div data-testid="home-page">Home</div>;
+  return (
+    <section data-testid="home-page">
+      <figure>
+        <img src={sushiImg} alt="" />
+      </figure>
+      <h1>Hi Sharonina!</h1>
+      <p>
+        Let's create a fantastic dish for our customers sharing our passion for
+        food, culture and innovation.
+      </p>
+      <p>Thank you for being a part of our team!</p>
+    </section>
+  );
 };
 
 export default Home;
