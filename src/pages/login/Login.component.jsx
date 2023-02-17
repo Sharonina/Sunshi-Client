9395aca83a0c6352ebc37672db897582ef807d01
Sharon Arana <shar_mdza@hotmail.com>
2023-02-17 10:50:13 -0600

    add settings for login

--- src/pages/login/Login.component.jsx
+++ src/pages/login/Login.component.jsx
@@ -3,6 +3,20 @@ import styles from "./Login.module.styl";
 import logo from "@/assets/sunshi1.png";
 
 const Login = () => {
+  const funcion = async () => {
+    const myURL = "https://burguerqueenapi.onrender.com/users/login";
+    const data = {
+      email: "shadmin@gmail.com",
+      password: "luckychan",
+    };
+    const response = await fetch(myURL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    });
+    const json = await response.json();
+    console.log(json);
+  };
   return (
     <section data-testid="login-page">
       <figure>
@@ -16,7 +30,7 @@ const Login = () => {
         <input placeholder="**********" type="password" />
         <p className={styles.errorMessage}>Please enter a password</p>
       </div>
-      <button>Sign in</button>
+      <button onClick={funcion}>Sign in</button>
     </section>
   );
 };
